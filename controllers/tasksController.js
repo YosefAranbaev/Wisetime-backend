3f21f1164057479ba160ddd1dc61006d77b8258f
yosef <aranbaev00@gmail.com>
2021-12-21 14:23:06 +0200

    backend with validation

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,38 +1,40 @@
 const Task = require('../models/tasks');
 
+const getDayName = (day) => {
+    switch(day) {
+        case 0: 
+            return 'sunday';
+        case 1:
+            return 'monday';
+        case 2: 
+            return 'tuesday';
+        case 3:
+            return 'wednesday';
+        case 4:
+            return 'thursday'; 
+        case 5:
+            return 'friday';
+        case 6:
+            return 'saturday'; 
+    }
+}
+
 exports.tasksController = {
     getTasks(req, res) {
-        console.log("docs");
         Task.find({})
             .then(docs => {
-                console.log(docs);
                 res.status(200); res.json(docs);
             })
             .catch(err => { res.status(400); res.json(`Error getting the data from db: ${err}`) });
     },
-//     putFlight(req, res) {
-//         const { body } = req;
-//         Expense.updateOne({ _id: getMemberid(req.url) }, { amount: body.amount})
-//             .then(docs => {
-//                 res.status(200); res.json(docs);
-//             })
-//             .catch(err => { res.status(400); res.json(`Error updating the data from db: ${err}`) });
-//     },
-//     deleteFlight(req, res) {
-//         Expense.deleteOne({ _id: getMemberid(req.url) })
-//             .then(docs => {
-//                 res.status(200); res.json(docs);
-//             })
-//             .catch(err => { res.status(400); res.json(`Error deleting the data from db: ${err}`) });
-//     },
     addTask(req, res) {
         const { body } = req;
-
-        console.log(body);
-
-        if (!(body.name && body.day && body.hour_start_time &&  body.hour_end_time && body.day && body.color)) {
-            res.status(500).send("Error saving an Outcome");
+        if (!(body.name && body.hour_start_time &&  body.hour_end_time && body.day && body.color)) {
+            res.status(401).send("Error saving Task");
         } else {
+            const date = new Date(body.day);
+            const day = getDayName(date.getDay());
+
             const newTask = new Task({
                 "name": body.name,
                 "duration_time": body.duration_time,
@@ -41,10 +43,8 @@ exports.tasksController = {
                 "id_of_side_user":  body.id_of_side_user,
                 "hour_start_time":  body.hour_start_time,
                 "hour_end_time":  body.hour_end_time,
-                "color": body.color
+                "color":  body.color
             });
-            console.log(newTask);
-            
             Task.find({})
             .then(docs => {
                 let is_friction=0;
@@ -58,14 +58,13 @@ exports.tasksController = {
                 if(is_friction==0)
                 newTask.save().then(result => {
                     if (result) {
-                        res.status(200).redirect('http://127.0.0.1:5500/wisetime-frontend/home.html')
-                        // res.send("hello");
+                        res.status(200).redirect('http://127.0.0.1:5500/wisetime-frontend/home.html');
                     } else {
                         res.status(500).send("Error saving a Outcome");
                     }
                 });
                 else{
-                    res.status(500).send("There is schedule frictions");
+                    res.status(403).send("There is schedule frictions");
                 }
             })
             .catch(err => { res.status(400); res.json(`Error getting the data from db: ${err}`) });
