6faf137b1c1dc2e533d759556ae55b3348c3e319
karina <karina4nosenko@gmail.com>
2022-01-15 00:10:51 +0200

    Fixed cors issue

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 exports.verifyToken = (req, res, next) => {
-  const token = req.headers['access-token'];
+  console.log(req.headers)
+  const token = req.headers.authorization.split(' ')[1];
 
   if (!token) {
     return res.status(403).json({'error': 'No token provided'});
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const { taskRouter } = require('./routers/taskRouter');
 const { constraintRouter } = require('./routers/constraintRouter');
@@ -18,12 +19,7 @@ if(process.env.ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.use(cors());
 
 app.use('/api/auth', authRouter);
 
